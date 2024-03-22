e863184048c106910fe9222ba429049a9257e821
michelbdg <114735296+michelbdg@users.noreply.github.com>
2024-03-22 11:59:00 +0100

    changement de nom du fichier App

---
+++ src/Pages/Home/Home.tsx
@@ -0,0 +1,16 @@
+import Card from "../../Components/card.tsx";
+import Header from "../../Components/header.tsx";
+import Hero from "../../Components/hero.tsx";
+ 
+function Home() {
+  return (
+    <>
+      <Header/>
+      <Hero/>
+      <Card/>
+
+    </>
+  );
+}
+
+export default Home;
